514dbe5d5cf6845700ffc00e9a981020e6482bf7
Nikolay Hadzhiev <nhadzhiev99@gmail.com>
2023-11-22 12:14:34 +0200

    refactor (App.tsx): integrate authentication and user data fetching
    - Implemented authentication state using react-firebase-hooks/auth.
    - Integrated user data fetching from Firebase for authenticated users.
    - Reorganized App component structure for better code organization.
    - Utilized React Router's Routes and Route components for improved navigation.
    - Created AuthenticatedRoute higher-order component for protected routes.
    - Adjusted useEffect dependency array for optimal dependency tracking.

--- WeTalk/src/App.tsx
+++ WeTalk/src/App.tsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './config/firebase-config';
+import { getUserData } from './services/users.service';
+import { useEffect, useState } from 'react';
+import { IAppState, IUserData } from './common/types';
+import AppContext from './context/AuthContext';
+import { Route, Routes } from 'react-router-dom';
+import LandingPageView from './views/LandingPage/LandingPageView';
+import Home from './views/Home/Home';
+import AuthenticatedRoute from './hoc/AuthenticatedRoute';
+// import Register from './components/Auth/Register/Register';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [user] = useAuthState(auth);
+  const [appState, setAppState] = useState<IAppState>({
+    user,
+    userData: null,
+  });
+
+  if (appState.user !== user) {
+    setAppState({ user, userData: null });
+  }
+
+  useEffect(() => {
+    if (user === null || user === undefined) return;
+
+    getUserData(user.uid)
+      .then((snapshot) => {
+        if (!snapshot.exists()) {
+          throw new Error('Invalid user!');
+        }
+
+        const userData: IUserData =
+          snapshot.val()[Object.keys(snapshot.val())[0]];
+
+        setAppState({
+          ...appState,
+          userData,
+        });
+      })
+      .catch((e) => console.error(e.message));
+  }, [appState, user]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AppContext.Provider value={{ ...appState, setContext: setAppState }}>
+        <Routes>
+          {user === null && <Route path="/" element={<LandingPageView />} />}
+          {user !== null && (
+            <Route
+              path="/"
+              element={
+                <AuthenticatedRoute>
+                  <Home />
+                </AuthenticatedRoute>
+              }
+            />
+          )}
+          {user !== null && (
+            <Route
+              path="/home"
+              element={
+                <AuthenticatedRoute>
+                  <Home />
+                </AuthenticatedRoute>
+              }
+            />
+          )}
+        </Routes>
+      </AppContext.Provider>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
