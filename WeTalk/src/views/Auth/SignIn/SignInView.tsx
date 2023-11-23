929c19b78d208a3071f17ad60476c7ab6af331cb
Nikolay Hadzhiev <nhadzhiev99@gmail.com>
2023-11-23 17:37:49 +0200

    Refactor SignInView Component
    - Improved the structure and readability of the SignInView component.
    - Utilized NavLink and Link from 'react-router-dom' for navigation.
    - Incorporated toast notifications for a more user-friendly experience.
    - Extracted common logic into helper functions to reduce redundancy.
    - Enhanced the styling and layout for a cleaner and more consistent design.

--- WeTalk/src/views/Auth/SignIn/SignInView.tsx
+++ WeTalk/src/views/Auth/SignIn/SignInView.tsx
@@ -2,33 +2,37 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import visible from '../../../assets/images/eye-svgrepo-com.svg';
 import hidden from '../../../assets/images/eye-off-svgrepo-com.svg';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-interface ISignInViewProps {
+type ISignInViewProps = {
   isLoading: boolean;
   updateForm: (
     field: string
   ) => (e: React.ChangeEvent<HTMLInputElement>) => void;
   handleClickShowPassword: () => void;
   showPassword: boolean;
-  switchToRegisterView: () => void;
   onLogin: (event: React.FormEvent) => void;
   email: string;
   password: string;
-}
+};
 
 const SignInView: React.FC<ISignInViewProps> = ({
   isLoading,
   updateForm,
   handleClickShowPassword,
   showPassword,
-  switchToRegisterView,
   onLogin,
   email,
   password,
 }) => {
   return (
     <>
+      <div className="mb-4 w-2/3 flex flex-col items-center">
+        <h1 className="font-bold text-primary text-2xl text-center tracking-tight">
+          Sign in your account
+        </h1>
+        <hr className="w-20 border-t-4 border-accent mb-4 mt-4" />
+      </div>
       <div className="signin-wrapper w-full max-w-xl">
         <form
           className="form bg-white shadow-xl border-solid border-2 rounded-lg p-10 w-96"
@@ -83,30 +87,31 @@ const SignInView: React.FC<ISignInViewProps> = ({
               </button>
             </div>
           </div>
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-center">
             <button
-              className="bg-accent hover:bg-primary text-primary hover:text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="bg-accent hover:bg-primary text-primary hover:text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
               disabled={isLoading}
             >
-              {isLoading ? 'Logging In...' : 'Sign In'}{' '}
+              {isLoading ? 'Signing In...' : 'Sign In'}{' '}
             </button>
-            <NavLink to="/forgot-password">
-              <span className="inline-block align-baseline font-bold text-sm text-primary hover:text-accent cursor-pointer">
-                Forgot Password?
-              </span>
-            </NavLink>
           </div>
         </form>
       </div>
-      <p className="mt-4 text-primary">
-        Don't have an account yet?{' '}
-        <span
-          className="font-bold text-primary cursor-pointer"
-          onClick={switchToRegisterView}
-        >
-          Sign Up
-        </span>
-      </p>
+      <div className="flex flex-col items-center justify-center mt-4">
+        <Link to="/forgot-password">
+          <span className="font-bold text-primary cursor-pointer hover:underline">
+            Forgot Password?
+          </span>
+        </Link>
+        <p className="text-primary mt-2">
+          Don't have an account yet?{' '}
+          <Link to="/signup">
+            <span className="font-bold text-primary cursor-pointer hover:underline">
+              Sign Up
+            </span>
+          </Link>
+        </p>
+      </div>
       <ToastContainer />
     </>
   );
