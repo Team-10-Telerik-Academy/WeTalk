8a0adffbdf038f1e3b04c5ebaa66ba6b861ba5f2
Nikolay Hadzhiev <nhadzhiev99@gmail.com>
2023-11-23 17:30:58 +0200

    Refactor AuthenticatedRoute for Improved Navigation Logic
    - Restructured the AuthenticatedRoute component to enhance navigation logic.
    - Simplified conditions for a clearer understanding of routing behavior.
    - Utilized React Router's Outlet and Navigate components for streamlined navigation.

--- WeTalk/src/hoc/AuthenticatedRoute.tsx
+++ WeTalk/src/hoc/AuthenticatedRoute.tsx
@@ -1,17 +1,50 @@
 import { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
-import { IChildrenProps } from '../common/types';
+// import { Navigate, useLocation } from 'react-router-dom';
+// import { IChildrenProps } from '../common/types';
+// import AppContext from '../context/AuthContext';
+
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import AppContext from '../context/AuthContext';
 
-export const AuthenticatedRoute: React.FC<IChildrenProps> = ({ children }) => {
+// export const AuthenticatedRoute: React.FC<IChildrenProps> = ({ children }) => {
+//   const context = useContext(AppContext);
+//   const location = useLocation();
+
+//   if (context?.user === null && location.pathname === '/signin') {
+//     return children;
+//   } else if (context?.user !== null && location.pathname === '/signin') {
+//     return <Navigate to="/" replace />;
+//   } else if (context?.user === null) {
+//     return <Navigate to="/signin" replace />;
+//   }
+
+//   return null;
+// };
+
+// export default AuthenticatedRoute;
+
+const AuthenticatedRoute = () => {
   const context = useContext(AppContext);
-  //   const location = useLocation();
+  const location = useLocation();
+  const toAuth =
+    location.pathname === '/signin' || location.pathname === '/signup';
 
-  if (context?.user === null) {
-    return <Navigate to="/" replace />;
+  // console.log(toAuth);
+  // console.log(context?.user);
+
+  if (!context?.user) {
+    return toAuth ? (
+      <Outlet />
+    ) : (
+      <Navigate to="/signin" replace state={location.pathname} />
+    );
+  } else {
+    return toAuth ? (
+      <Navigate to="/home" replace state={location.pathname} />
+    ) : (
+      <Outlet />
+    );
   }
-
-  return children;
 };
 
 export default AuthenticatedRoute;
