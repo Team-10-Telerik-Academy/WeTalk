763792b04f74046fc7b4f3891660b18e20abddb3
Nikolay Hadzhiev <nhadzhiev99@gmail.com>
2023-11-24 19:52:31 +0200

    refactor: Extract SearchBar from main sidebar to standalone component
    - Move the SearchBar code to its own component, SearchBarView
    - Enhance modularity and readability in the main sidebar structure
    - Improve code organization for better maintainability

---
+++ WeTalk/src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,11 @@
+import SearchBarView from '../../views/SearchBar/SearchBarView';
+
+const SearchBar = () => {
+  return (
+    <>
+      <SearchBarView />
+    </>
+  );
+};
+
+export default SearchBar;
