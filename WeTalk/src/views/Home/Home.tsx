249085520fc66ee17721aebcfbcb4db325d7003f
DianaAlemkova <diana.alemkova@gmail.com>
2023-11-24 14:21:37 +0200

    feat: Update Home component

--- WeTalk/src/views/Home/Home.tsx
+++ WeTalk/src/views/Home/Home.tsx
@@ -8,6 +8,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import AppContext from '../../context/AuthContext';
 import { IAppContext, IAppState } from '../../common/types';
 import nhAvatar from '../../assets/images/avatar-NH.jpg';
+import SearchUsers from '../../components/SearchUsers/SearchUsers';
 // import ThemeButton from '../../components/ThemeButton/ThemeButton';
 
 export default function Home() {
@@ -268,21 +269,21 @@ export default function Home() {
                 viewBox="0 0 24 24"
                 fill="none"
               >
-                <path
+                {/* <path
                   d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
                   stroke="currentColor"
                   strokeWidth="2"
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                ></path>
+                ></path> */}
               </svg>
             </span>
-
-            <input
+            <SearchUsers />
+            {/* <input
               type="text"
               className="w-full py-1.5 pl-10 pr-4 text-gray-700 bg-secondary border rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
               placeholder="Search"
-            />
+            /> */}
           </div>
 
           <nav className="mt-6 -mx-3 space-y-6">
