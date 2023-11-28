124e99a0a6694baf0f08b389ecf5ae556c037ca5
Nikolay Hadzhiev <nhadzhiev99@gmail.com>
2023-11-28 14:51:34 +0200

    feat: Introduce MembersModal component
    - Created a new component for displaying team members in a modal.
    - Utilized FontAwesome icons for a clean and consistent UI.
    - Incorporated user profile information, including avatars and status indicators.
    - Added a crown icon to identify the team owner.
    - Implemented functionality to remove members, restricted to the team owner.
    - Designed the modal with a responsive layout for various screen sizes.
    - Enhanced the overall look and feel with appropriate styling and spacing.

---
+++ WeTalk/src/components/MainSidebar/Teams/MembersModal.tsx
@@ -0,0 +1,103 @@
+import { useContext } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan } from '@fortawesome/free-regular-svg-icons';
+import { faCrown } from '@fortawesome/free-solid-svg-icons';
+import nhAvatar from '../../../assets/images/avatar-NH.jpg';
+import { IAppContext, ITeam } from '../../../common/types';
+import AppContext from '../../../context/AuthContext';
+import Status from '../../Profile/Status';
+import Profile from '../../Profile/Profile';
+
+type IUser = {
+  handle: string;
+  firstName: string;
+  lastName: string;
+};
+
+type MembersModalProps = {
+  teamData: ITeam;
+  users: IUser[];
+  isOpen: boolean;
+  onClose: () => void;
+  onRemoveMember: (selectedMember: string) => void;
+};
+
+const MembersModal: React.FC<MembersModalProps> = ({
+  teamData,
+  users,
+  isOpen,
+  onClose,
+  onRemoveMember,
+}) => {
+  const { userData } = useContext(AppContext) as IAppContext;
+
+  return (
+    <>
+      {isOpen && (
+        <div className="fixed inset-0 z-10 overflow-y-auto bg-black bg-opacity-50">
+          <div className="flex items-center justify-center min-h-screen xl:py-10">
+            <div className="bg-white w-full md:w-1/2 lg:w-1/3 2xl:w-1/4 pt-6 rounded shadow-lg">
+              <div className="flex flex-col items-center justify-center mb-8">
+                <h2 className="text-xl text-primary font-bold text-center">
+                  {teamData.teamName} Members
+                </h2>
+                <hr className="w-12 border-t-4 border-accent mt-2" />
+              </div>
+              <div className="space-y-2 p-2 rounded mb-10">
+                {teamData.members.map((member) => (
+                  <div
+                    key={member}
+                    className="flex items-center justify-between border border-primary rounded p-2 hover:bg-primary hover:bg-opacity-5"
+                  >
+                    <div className="flex items-center py-1">
+                      {users
+                        .filter((user) => user.handle === member)
+                        .map((user) => (
+                          <>
+                            <div className="flex gap-2 items-center">
+                              <Profile handle={user.handle} />
+                              <div
+                                key={user.handle}
+                                className="text-xs md:text-sm text-primary"
+                              >
+                                {user.firstName} {user.lastName} ({user.handle})
+                              </div>
+                            </div>
+                            <span className="font-bold text-primary text-xs md:text-sm">
+                              {member === teamData.owner && (
+                                <FontAwesomeIcon
+                                  icon={faCrown}
+                                  className="text- cursor-pointer px-2 text-xs md:text-sm"
+                                />
+                              )}
+                            </span>
+                          </>
+                        ))}
+                    </div>
+                    {userData?.handle === teamData.owner && (
+                      <FontAwesomeIcon
+                        icon={faTrashCan}
+                        className="text-primary cursor-pointer px-2 text-xs md:text-lg"
+                        onClick={() => onRemoveMember(member)}
+                      />
+                    )}
+                  </div>
+                ))}
+              </div>
+              <div className="flex items-center justify-center">
+                <button
+                  className="bg-accent text-primary font-bold px-4 py-2 rounded hover:bg-primary hover:text-secondary w-full"
+                  onClick={onClose}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default MembersModal;
