21fc0be83bef7194c2fbe4bfc92ca90add4ae3a7
Nikolay Hadzhiev <nhadzhiev99@gmail.com>
2023-11-28 14:18:03 +0200

    refactor: Update team-related functions
    - Make `createTeam` and `addTeamToUser` asynchronous.
    - Replace `getAllTeamsByName` with `getAllTeams` for real-time updates.
    - Introduce real-time updates for `getTeamByTeamName`.
    - Modify `removeTeamFromUsers` and `deleteTeam` to use async/await.
    - Add functions for removing a team from a user and removing a member from a team.
    - Introduce functions for adding and removing members from a team.
    - Implement the ability to update a team's name.

--- WeTalk/src/services/teams.service.ts
+++ WeTalk/src/services/teams.service.ts
@@ -1,5 +1,14 @@
-import { DataSnapshot, get, ref, remove, set, update } from 'firebase/database';
+import {
+  DataSnapshot,
+  get,
+  ref,
+  remove,
+  set,
+  update,
+  onValue,
+} from 'firebase/database';
 import { db } from '../config/firebase-config';
+import { ITeam } from '../common/types';
 
 export const fromTeamsDocument = (snapshot: DataSnapshot) => {
   const teamsDocument = snapshot.val();
@@ -27,8 +36,8 @@ export const createTeam = async (
     createdOn: Date.now(),
     channels: {},
   });
-  addTeamToUser(owner, teamName);
-  members.forEach((member) => addTeamToUser(member, teamName));
+  await addTeamToUser(owner, teamName);
+  members.forEach(async (member) => await addTeamToUser(member, teamName));
 };
 
 export const addTeamToUser = async (handle: string, teamName: string) => {
@@ -46,25 +55,28 @@ export const addTeamToUser = async (handle: string, teamName: string) => {
   }
 };
 
-export const getTeamByTeamName = (teamName: string) => {
-  return get(ref(db, `teams/${teamName}`));
+export const getTeamByTeamName = (
+  teamName: string,
+  callback: (teamData: DataSnapshot | null) => void
+) => {
+  const teamRef = ref(db, `teams/${teamName}`);
+  onValue(teamRef, (snapshot) => {
+    callback(snapshot.exists() ? snapshot : null);
+  });
 };
 
-export const getAllTeamsByName = async () => {
-  try {
-    const userRef = ref(db, `teams`);
-    const userSnapshot = await get(userRef);
+export const getAllTeams = (callback: (teamsArray: ITeam[]) => void) => {
+  const teamsRef = ref(db, 'teams');
 
-    if (userSnapshot.exists()) {
-      const usersData = userSnapshot.val();
-      const usersArray = Object.keys(usersData).map((teamName) => teamName);
-      return usersArray;
-    } else {
-      throw new Error(`Users not found`);
-    }
-  } catch (error) {
-    console.error(error);
-  }
+  const unsubscribe = onValue(teamsRef, (snapshot) => {
+    const teamsData = snapshot.val();
+    const teamsArray = Object.values(teamsData || {}).map(
+      (teamData) => teamData as ITeam
+    );
+    callback(teamsArray);
+  });
+
+  return unsubscribe;
 };
 
 const removeTeamFromUsers = async (teamName: string, members: string[]) => {
@@ -87,9 +99,110 @@ export const deleteTeam = async (teamName: string, owner: string) => {
 
   if (teamData && teamData.owner === owner) {
     await removeTeamFromUsers(teamName, teamData.members);
-
     await remove(ref(db, `teams/${teamName}`));
   } else {
     console.error("You don't have permission to delete this team!");
+  }
+};
+
+export const removeTeamFromUser = async (
+  teamName: string,
+  memberToRemove: string
+) => {
+  try {
+    const userTeamsRef = ref(db, `users/${memberToRemove}/teams`);
+    const userTeamsSnapshot = await get(userTeamsRef);
+
+    if (userTeamsSnapshot.exists()) {
+      const updatedTeams = userTeamsSnapshot
+        .val()
+        .filter((team: string) => team !== teamName);
+      await set(ref(db, `users/${memberToRemove}/teams`), updatedTeams);
+    }
+  } catch (error) {
+    console.error('Error removing team from user', error);
+  }
+};
+
+export const removeMemberFromTeam = async (
+  teamName: string,
+  memberToRemove: string
+) => {
+  try {
+    const teamSnapshot = await get(ref(db, `teams/${teamName}`));
+
+    if (teamSnapshot.exists()) {
+      const teamData = teamSnapshot.val();
+
+      if (teamData.members.includes(memberToRemove)) {
+        const updateMembers = teamData.members.filter(
+          (member: string) => member !== memberToRemove
+        );
+
+        await removeTeamFromUser(teamName, memberToRemove);
+        await update(ref(db, `teams/${teamName}`), {
+          members: updateMembers,
+        });
+      } else {
+        throw new Error('Member not found in the team!');
+      }
+    } else {
+      throw new Error('Team not found!');
+    }
+  } catch (error) {
+    console.error('Error removing member from team:', error);
+  }
+};
+
+export const addMembersToTeam = async (teamName: string, members: string[]) => {
+  try {
+    const teamRef = ref(db, `teams/${teamName}/members`);
+    const teamSnapshot = await get(teamRef);
+
+    if (teamSnapshot.exists()) {
+      const currentMembers = teamSnapshot.val();
+      const updatedMembers = [...currentMembers, ...members];
+
+      members.forEach(async (member) => await addTeamToUser(member, teamName));
+      await set(teamRef, updatedMembers);
+    }
+  } catch (error) {
+    console.error('Error adding members to team:', error);
+  }
+};
+
+export const updateTeamName = async (
+  currentTeamName: string,
+  newTeamName: string
+) => {
+  try {
+    const teamSnapshot = await get(ref(db, `teams/${currentTeamName}`));
+
+    if (teamSnapshot.exists()) {
+      const teamData = teamSnapshot.val();
+
+      await update(ref(db, `teams/${currentTeamName}`), {
+        teamName: newTeamName,
+      });
+
+      const teamMembers = teamData.members || [];
+      teamMembers.forEach(async (member: string) => {
+        const userTeamsRef = ref(db, `users/${member}/teams`);
+        const userTeamsSnapshot = await get(userTeamsRef);
+
+        if (userTeamsSnapshot.exists()) {
+          const updatedTeams = userTeamsSnapshot
+            .val()
+            .map((team: string) =>
+              team === currentTeamName ? newTeamName : team
+            );
+          await set(ref(db, `users/${member}/teams`), updatedTeams);
+        }
+      });
+    } else {
+      throw new Error('Team not found!');
+    }
+  } catch (error) {
+    console.error('Error updating team name:', error);
   }
 };
