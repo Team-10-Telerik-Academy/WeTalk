f983d2988d871d6d6aa5204f45aa42f9b511be77
Nikolay Hadzhiev <nhadzhiev99@gmail.com>
2023-11-28 14:50:41 +0200

    refactor: Update TeamsView component
    - Changed the type of the `teams` prop from `string[]` to `ITeam[]` to align with the structure of team data.
    - Added new prop types (`onRemoveMember`, `onAddMembersToTeam`, `onSaveTeamName`) to handle additional team operations.
    - Added context to fetch the `userData` using `useContext(AppContext) as IAppContext` to access user information.
    - Updated the styling of the component, including font sizes and label styles, to improve readability.

--- WeTalk/src/views/MainSidebar/Teams/TeamsView.tsx
+++ WeTalk/src/views/MainSidebar/Teams/TeamsView.tsx
@@ -1,23 +1,38 @@
 import CreateTeam from '../../../components/MainSidebar/Teams/CreateTeam';
 import SingleTeam from '../../../components/MainSidebar/Teams/SingleTeam';
+import { useContext } from 'react';
+import AppContext from '../../../context/AuthContext';
+import { IAppContext, ITeam } from '../../../common/types';
 
 type ITeamsViewProps = {
   onCreateTeam: (teamName: string, members: string[]) => void;
-  teams: string[];
+  teams: ITeam[];
   onDeleteTeam: (teamName: string, owner: string) => void;
+  onRemoveMember: (
+    teamName: string,
+    owner: string,
+    memberToRemove: string
+  ) => void;
+  onAddMembersToTeam: (teamName: string, members: string[]) => void;
+  onSaveTeamName: (teamData: ITeam, newName: string) => void;
 };
 
 const TeamsView: React.FC<ITeamsViewProps> = ({
   onCreateTeam,
   teams,
   onDeleteTeam,
+  onRemoveMember,
+  onAddMembersToTeam,
+  onSaveTeamName,
 }) => {
+  const { userData } = useContext(AppContext) as IAppContext;
+
   return (
     <>
       <nav className="mt-6 -mx-3 space-y-4">
         <div className="space-y-2">
           <div className="flex items-center justify-between px-4">
-            <label className="text-primary text-2xl font-extrabold tracking-tight uppercase dark:text-gray-400">
+            <label className="text-primary text-xl md:text-2xl xl:text-3xl font-extrabold tracking-tight uppercase dark:text-gray-400">
               Teams
             </label>
             <div className="flex items-center justify-between">
@@ -26,15 +41,23 @@ const TeamsView: React.FC<ITeamsViewProps> = ({
           </div>
         </div>
         <hr className="mt-4" />
-        <p className="px-7 text-gray-500">My teams</p>
-        <div className="px-3 space-y-4">
-          {teams.map((teamName) => (
-            <SingleTeam
-              key={teamName}
-              teamName={teamName}
-              onDeleteTeam={onDeleteTeam}
-            />
-          ))}
+        <p className="px-7 text-gray-500 text-sm lg:text-[16px]">My teams</p>
+        <div className="px-3">
+          {teams &&
+            teams.map(
+              (team) =>
+                Array.isArray(team.members) &&
+                team.members.includes(userData?.handle ?? '') && (
+                  <SingleTeam
+                    key={team.teamId}
+                    teamName={team.teamName}
+                    onDeleteTeam={onDeleteTeam}
+                    onRemoveMember={onRemoveMember}
+                    onAddMembersToTeam={onAddMembersToTeam}
+                    onSaveTeamName={onSaveTeamName}
+                  />
+                )
+            )}
         </div>
       </nav>
     </>
