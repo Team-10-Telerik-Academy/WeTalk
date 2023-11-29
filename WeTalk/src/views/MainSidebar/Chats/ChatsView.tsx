f1d7927cc3fca5faf24b376ce37e235c050b5c68
Atanas <atanasgeorgiev444@gmail.com>
2023-11-29 12:57:57 +0200

    feat: Add component to create chats with users
    - With this feature the user now can add other users to chat with them
    - The chat can be one on one or group chat
    - You are allowed to create a name for the chat

--- WeTalk/src/views/MainSidebar/Chats/ChatsView.tsx
+++ WeTalk/src/views/MainSidebar/Chats/ChatsView.tsx
@@ -1,25 +1,95 @@
+import { useEffect, useState } from "react";
+import CreateNewChat from "../../../components/CreateChat/CreateChat";
+import { getAllChats, onChatUpdate } from "../../../services/chat.service";
+import { IAppContext } from "../../../common/types";
+import AppContext from "../../../context/AuthContext";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import Profile from "../../../components/Profile/Profile";
+
+type IChatData = {
+  chatId: string;
+  chatName: string;
+  createdOn: number;
+  members: string[];
+};
+
 const ChatsView = () => {
+  const [chats, setChats] = useState<IChatData[]>([]);
+  const { userData } = useContext(AppContext) as IAppContext;
+
+  useEffect(() => {
+    const fetchChats = async () => {
+      try {
+        const chatData = await getAllChats();
+        setChats(chatData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchChats();
+
+    // const unsubscribe = onChatUpdate(userData?.handle || "", (updatedChats) => {
+    //   setChats(updatedChats);
+    // });
+
+    // return () => {
+    //   unsubscribe();
+    // };
+  }, [userData]);
+
+  const userChats = chats.filter((chat) =>
+    chat.members.includes(userData?.handle || "")
+  );
+
   return (
-    <>
-      <nav className="mt-6 -mx-3 space-y-6">
+    <div className="flex w-1/4 px-4 border-r">
+      <nav className="mt-6 -mx-3 space-y-6 w-full">
         <div className="space-y-2">
-          <div className="flex items-center justify-between px-3 font-bold ">
+          <div className="flex items-center justify-between px-3 font-bold">
             <label className="text-primary text-2xl dark:text-gray-400">
               Chats
             </label>
-            <button className="text-primary text-lg text-center w-8 shadow-md rounded-lg border-primary border-2 px-1 hover:bg-primary hover:text-secondary">
-              +
-            </button>
+            <CreateNewChat />
           </div>
         </div>
         <hr className="mt-4" />
         <div>
-          <p className="px-3 text-gray-500 tracking-wide">#Chat1</p>
-          <p className="px-3 text-gray-500 tracking-wide">#Chat2</p>
-          <p className="px-3 text-gray-500 tracking-wide">#Chat3</p>
+          {userChats.map((chat) => (
+            <div key={chat.chatId}>
+              <Link
+                to={`${chat.chatId}`}
+                className="text-gray-500 tracking-wide"
+              >
+                <div className="flex bg-secondary my-2 border-b">
+                  <div className="flex flex-row">
+                    {chat?.members.length > 2 ? (
+                      <p className="pr-2 text-primary">group</p>
+                    ) : (
+                      <div>
+                        {chat.members
+                          .filter((member) => member !== userData?.handle)
+                          .map((member) => (
+                            <Profile key={member} handle={member} />
+                          ))}
+                      </div>
+                    )}
+                    <div className="text-primary">
+                      {chat?.members.length > 2
+                        ? chat?.chatName
+                        : chat?.members[0] !== userData?.handle
+                        ? chat?.members[0]
+                        : chat?.members[1]}
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
         </div>
       </nav>
-    </>
+    </div>
   );
 };
 
