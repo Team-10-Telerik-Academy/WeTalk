aef49ce89226fee7591adaf5f3641afed0739dd8
Nikolay Hadzhiev <nhadzhiev99@gmail.com>
2023-12-04 17:54:01 +0200

    refactor: Update TeamsView component
    - Added a SearchBar component.
    - Adjusted styling for better responsiveness.
    - Removed a commented-out line.
    - Updated the label styling for consistency.
    - Changed the layout for the 'CreateTeam' component.
    - Removed a commented-out line for 'My teams'.
    - Applied additional styling adjustments.

--- WeTalk/src/views/MainSidebar/Teams/TeamsView.tsx
+++ WeTalk/src/views/MainSidebar/Teams/TeamsView.tsx
@@ -2,7 +2,8 @@ import CreateTeam from '../../../components/MainSidebar/Teams/CreateTeam';
 import SingleTeam from '../../../components/MainSidebar/Teams/SingleTeam';
 import { useContext } from 'react';
 import AppContext from '../../../context/AuthContext';
-import { IAppContext, ITeam } from '../../../common/types';
+import { IAppContext, ITeam, IUserData } from '../../../common/types';
+import SearchBar from '../../../components/SearchBar/SearchBar';
 
 type ITeamsViewProps = {
   onCreateTeam: (teamName: string, members: string[]) => void;
@@ -15,6 +16,7 @@ type ITeamsViewProps = {
   ) => void;
   onAddMembersToTeam: (teamName: string, members: string[]) => void;
   onSaveTeamName: (teamData: ITeam, newName: string) => void;
+  users: IUserData[];
 };
 
 const TeamsView: React.FC<ITeamsViewProps> = ({
@@ -24,24 +26,30 @@ const TeamsView: React.FC<ITeamsViewProps> = ({
   onRemoveMember,
   onAddMembersToTeam,
   onSaveTeamName,
+  users,
 }) => {
   const { userData } = useContext(AppContext) as IAppContext;
 
   return (
-    <>
-      <nav className="mt-6 -mx-3 space-y-4">
+    <div className="min-h-screen sm:min-h-screen md:min-h-screen lg:min-h-screen px-4 py-8 border-r overflow-y-auto bg-secondary w-full sm:w-1/3 md:w-1/3 lg:w-[3000px] xl:w-[3000px] 2xl:w-full dark:bg-gray-900 dark:border-gray-700">
+      <SearchBar />
+      <nav className="mt-6 space-y-4">
         <div className="space-y-2">
-          <div className="flex items-center justify-between px-4">
+          <div className="flex items-center justify-between">
             <label className="text-primary text-xl md:text-2xl xl:text-3xl font-extrabold tracking-tight uppercase dark:text-gray-400">
               Teams
             </label>
             <div className="flex items-center justify-between">
-              <CreateTeam onCreateTeam={onCreateTeam} teams={teams} />
+              <CreateTeam
+                onCreateTeam={onCreateTeam}
+                teams={teams}
+                users={users}
+              />
             </div>
           </div>
         </div>
         <hr className="mt-4" />
-        <p className="px-7 text-gray-500 text-sm lg:text-[16px]">My teams</p>
+        {/* <p className="px-7 text-gray-500 text-sm lg:text-[16px]">My teams</p> */}
         <div className="px-3">
           {teams &&
             teams.map(
@@ -55,12 +63,13 @@ const TeamsView: React.FC<ITeamsViewProps> = ({
                     onRemoveMember={onRemoveMember}
                     onAddMembersToTeam={onAddMembersToTeam}
                     onSaveTeamName={onSaveTeamName}
+                    users={users}
                   />
                 )
             )}
         </div>
       </nav>
-    </>
+    </div>
   );
 };
 
