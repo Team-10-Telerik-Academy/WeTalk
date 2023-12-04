e5e83d236ff91af7fc7444366e2b31902785a4f3
Nikolay Hadzhiev <nhadzhiev99@gmail.com>
2023-12-04 17:35:10 +0200

    refactor: Simplify structure in Chats component
    - Update the component to use a fragment for a cleaner syntax.
    - Improve readability by removing unnecessary <div> wrapper.

--- WeTalk/src/components/MainSidebar/Chats/Chats.tsx
+++ WeTalk/src/components/MainSidebar/Chats/Chats.tsx
@@ -1,12 +1,12 @@
-import { Outlet } from "react-router";
-import ChatsView from "../../../views/MainSidebar/Chats/ChatsView";
+import { Outlet } from 'react-router';
+import ChatsView from '../../../views/MainSidebar/Chats/ChatsView';
 
 const Chats = () => {
   return (
-    <div className="flex w-full">
+    <>
       <ChatsView />
       <Outlet />
-    </div>
+    </>
   );
 };
 
