081d213766e568c60c3639de7da7d3b45bccd013
Nikolay Hadzhiev <nhadzhiev99@gmail.com>
2023-12-04 17:42:28 +0200

    refactor & style: Enhance CreateTeam component styling and animations and Extract common logic to fetch users in Teams component
    - Introduce an "jump-in" animation for a smoother modal entrance.

--- WeTalk/src/components/MainSidebar/Teams/CreateTeam.tsx
+++ WeTalk/src/components/MainSidebar/Teams/CreateTeam.tsx
@@ -1,46 +1,32 @@
-import { useState, useEffect, useContext } from "react";
-import { getAllUsers } from "../../../services/users.service";
-import AppContext from "../../../context/AuthContext";
-import { faPlus, faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { IAppContext, ITeam, IUserData } from "../../../common/types";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { useState, useContext } from 'react';
+import AppContext from '../../../context/AuthContext';
+import { faPlus, faCheck, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IAppContext, ITeam, IUserData } from '../../../common/types';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import {
   MAX_TEAM_NAME_LENGTH,
   MIN_TEAM_NAME_LENGTH,
-} from "../../../common/constants";
+} from '../../../common/constants';
 
 type ICreateTeamProps = {
   onCreateTeam: (teamName: string, members: string[]) => void;
   teams: ITeam[];
+  users: IUserData[];
 };
 
-const CreateTeam: React.FC<ICreateTeamProps> = ({ onCreateTeam, teams }) => {
-  const [teamName, setTeamName] = useState("");
+const CreateTeam: React.FC<ICreateTeamProps> = ({
+  onCreateTeam,
+  teams,
+  users,
+}) => {
+  const [teamName, setTeamName] = useState('');
   const [members, setMembers] = useState<string[]>([]);
   const [selectAll, setSelectAll] = useState(false);
-  const [users, setUsers] = useState<IUserData[]>([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const { userData } = useContext(AppContext) as IAppContext;
-
-  useEffect(() => {
-    try {
-      const usersCallback = (usersData: IUserData[]) => {
-        setUsers(usersData);
-        console.log(usersData);
-      };
-
-      const unsubscribe = getAllUsers(usersCallback);
-
-      return () => {
-        unsubscribe();
-      };
-    } catch (error) {
-      console.error("Error fetching users", error);
-    }
-  }, []);
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -48,16 +34,16 @@ const CreateTeam: React.FC<ICreateTeamProps> = ({ onCreateTeam, teams }) => {
 
   const closeModal = () => {
     setIsModalOpen(false);
-    setTeamName("");
+    setTeamName('');
     setMembers([]);
     setSelectAll(false);
   };
 
   const handleCreateTeam = async () => {
     if (!teamName) {
-      toast.warning("Team Name is required!", {
+      toast.warning('Team Name is required!', {
         autoClose: 3000,
-        className: "font-bold",
+        className: 'font-bold',
       });
       return;
     }
@@ -70,7 +56,7 @@ const CreateTeam: React.FC<ICreateTeamProps> = ({ onCreateTeam, teams }) => {
         `Team Name must contain between ${MIN_TEAM_NAME_LENGTH} and ${MAX_TEAM_NAME_LENGTH} characters!`,
         {
           autoClose: 3000,
-          className: "font-bold",
+          className: 'font-bold',
         }
       );
       return;
@@ -79,9 +65,9 @@ const CreateTeam: React.FC<ICreateTeamProps> = ({ onCreateTeam, teams }) => {
     if (teams.some((team) => team.teamName === teamName)) {
       toast.warning(`Team with name ${teamName} already exists!`, {
         autoClose: 3000,
-        className: "font-bold",
+        className: 'font-bold',
       });
-      setTeamName("");
+      setTeamName('');
       setMembers([]);
       setSelectAll(false);
       return;
@@ -89,7 +75,7 @@ const CreateTeam: React.FC<ICreateTeamProps> = ({ onCreateTeam, teams }) => {
 
     onCreateTeam(teamName, members);
 
-    setTeamName("");
+    setTeamName('');
     setMembers([]);
     setSelectAll(false);
     closeModal();
@@ -132,7 +118,7 @@ const CreateTeam: React.FC<ICreateTeamProps> = ({ onCreateTeam, teams }) => {
         {isModalOpen && (
           <div className="fixed inset-0 z-10 overflow-y-auto bg-black bg-opacity-50">
             <div className="flex items-center justify-center min-h-screen xl:py-10">
-              <div className="bg-white w-full md:w-1/2 lg:w-1/3 2xl:w-1/4 p-6 rounded shadow-lg">
+              <div className="bg-white w-full md:w-1/2 lg:w-1/3 2xl:w-1/4 p-6 rounded shadow-lg animate-jump-in">
                 <div className="flex flex-col items-center justify-center mb-6">
                   <h2 className="text-xl text-primary font-bold">
                     Create Team
