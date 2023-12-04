97a640d25ab8cdc4fd48da64a074b9068203c8b7
Nikolay Hadzhiev <nhadzhiev99@gmail.com>
2023-12-04 17:56:38 +0200

    refactor: Update Home component
    - Removed unused imports and commented-out code.

--- WeTalk/src/views/Home/Home.tsx
+++ WeTalk/src/views/Home/Home.tsx
@@ -1,23 +1,13 @@
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
-import { logoutUser } from "../../services/auth.service";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../../config/firebase-config";
-import { useContext, useState } from "react";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import AppContext from "../../context/AuthContext";
-import { IAppContext, IAppState } from "../../common/types";
-// import NavigationSidebar from "../../components/NavigationSidebar/NavigationSidebar';
-import MainSidebar from "../../components/MainSidebar/MainSidebar";
-// import MainContent from '../../components/MainContent/MainContent";
-import Settings from "../../components/Profile/Settings";
-import Profile from "../../components/Profile/Profile";
-import SearchUsers from "../../components/SearchUsers/SearchUsers";
-import NavigationSidebarView from "../NavigationSidebar/NavigationSidebarView";
-import MainContent from "../../components/MainContent/MainContent";
-import SingleChat from "../Chat/SingleChatView";
-import SingleChatView from "../Chat/SingleChatView";
-// import ThemeButton from '../../components/ThemeButton/ThemeButton';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { logoutUser } from '../../services/auth.service';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../config/firebase-config';
+import { useContext, useState } from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import AppContext from '../../context/AuthContext';
+import { IAppContext, IAppState } from '../../common/types';
+import NavigationSidebarView from '../NavigationSidebar/NavigationSidebarView';
 
 export const Home = () => {
   const [user] = useAuthState(auth);
@@ -35,7 +25,6 @@ export const Home = () => {
   }
 
   const navigate = useNavigate();
-  const location = useLocation();
 
   const onLogout = () => {
     logoutUser(userData?.handle).then(() => {
@@ -54,25 +43,10 @@ export const Home = () => {
     });
   };
 
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen((prevIsSidebarOpen) => !prevIsSidebarOpen);
-  };
-
-  const isHomePage = location.pathname === '/home';
-
-  // const isChatsPage = location.pathname === "/home/chats/chat";
-
   return (
     <div className="flex w-full">
       <aside className="flex">
-        <NavigationSidebarView
-          onLogout={onLogout}
-          toggleSidebar={toggleSidebar}
-        />
-        {/* {!isHomePage && <MainSidebar isSidebarOpen={isSidebarOpen} />} */}
-        {/* <MainContent /> */}
+        <NavigationSidebarView onLogout={onLogout} />
       </aside>
       <Outlet />
     </div>
