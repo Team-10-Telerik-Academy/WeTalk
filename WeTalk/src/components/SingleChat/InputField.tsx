4cea4b8b28f2c7f10623d8c34a08e8cccf945637
Atanas <atanasgeorgiev444@gmail.com>
2023-12-08 14:54:18 +0200

    feat: Add giphy and send a picture service input
    - This feature adds to the input component the options to send a gif or
      a picture

--- WeTalk/src/components/SingleChat/InputField.tsx
+++ WeTalk/src/components/SingleChat/InputField.tsx
@@ -1,32 +1,54 @@
-import { ref, update } from 'firebase/database';
-import { SendMessage } from '../../services/chat.service';
-import EmojiInput from './EmojiInput';
-import { db } from '../../config/firebase-config';
+import { ref, update } from "firebase/database";
+import { SendMessage, sendFile } from "../../services/chat.service";
+import EmojiInput from "./EmojiInput";
+import { db } from "../../config/firebase-config";
+import { useEffect, useState } from "react";
+import Giphy from "./Giphy";
+import SendFile from "./SendFile";
 
 interface InputFieldProps {
   handle: string;
   chatId: string;
   setInputValue: React.Dispatch<React.SetStateAction<string>>;
   handleInputChange: (value: string) => void;
+  members: string[];
 }
 
 const InputField: React.FC<InputFieldProps> = ({
   handle,
   chatId,
+  members,
   setInputValue,
   handleInputChange,
 }) => {
+  // console.log(members);
+
+  // const handles = members.map((member) => member.handle);
+  // console.log(handles);
+
+  const filteredMembers = members.filter((member) => member !== handle);
+  console.log(handle);
+
   const handleSendMessage = async (message: string) => {
     try {
       // Set typing status to false before sending the message
       update(ref(db, `chats/${chatId}/typingStatus`), {
         [handle]: false,
       });
+      console.log("filtered", filteredMembers);
 
       // Send the message
-      await SendMessage(chatId, message, handle);
+      await SendMessage(chatId, message, handle, filteredMembers);
     } catch (error) {
       console.log(error);
+    }
+  };
+
+  const handleSaveFile = async (file: File) => {
+    try {
+      await sendFile(chatId, file, handle, filteredMembers);
+    } catch (e) {
+      console.log(e);
     }
   };
 
@@ -37,7 +59,9 @@ const InputField: React.FC<InputFieldProps> = ({
   };
 
   return (
-    <div className="flex">
+    <div className="flex items-center">
+      <Giphy chatId={chatId} handle={handle} members={members} />
+      <SendFile onSave={handleSaveFile} chatId={chatId} />
       <EmojiInput onSubmit={handleSendMessage} handleChange={handleChange} />
     </div>
   );
