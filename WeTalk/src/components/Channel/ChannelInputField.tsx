9a4507219860e588594eea8140cda29b6b8ef58f
DianaAlemkova <diana.alemkova@gmail.com>
2023-12-12 15:48:26 +0200

    feat. Channel input field

--- WeTalk/src/components/Channel/ChannelInputField.tsx
+++ WeTalk/src/components/Channel/ChannelInputField.tsx
@@ -1,47 +1,113 @@
 import { useState } from "react";
-import { sendChannelMessage } from "../../services/channel.service";
+import { sendMessage, sendFile } from "../../services/channel.service";
+import { ref, update } from "@firebase/database";
+import { db } from "../../config/firebase-config";
+import ChannelSendFile from "./ChannelSendFile";
+import ChannelGiphy from "./ChannelGiphy";
+import EmojiInput from "../SingleChat/EmojiInput";
 
- 
-const ChannelInputField = ({ handle, channelId }) => {
-  const [message, setMessage] = useState("");
- 
-  const handleInputChange = (event) => {
-    setMessage(event.target.value);
+
+interface InputFieldProps {
+    handle: string;
+    channelId: string;
+    setInputValue: React.Dispatch<React.SetStateAction<string>>;
+    handleInputChange: (value: string) => void;
+    members: string[];
+  }
+  
+  const ChannelInputField: React.FC<InputFieldProps> = ({
+    handle,
+    channelId,
+    members,
+    setInputValue,
+    handleInputChange,
+  }) => {
+    // console.log(members);
+  
+
+    const filteredMembers = members.filter((member) => member !== handle);
+    console.log(handle);
+  
+    const handleSendMessage = async (message: string) => {
+      try {
+        // Set typing status to false before sending the message
+        update(ref(db, `channels/${channelId}/typingStatus`), {
+          [handle]: false,
+        });
+        console.log("filtered", filteredMembers);
+  
+        // Send the message
+        await sendMessage(channelId, message, handle, filteredMembers);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    const handleSaveFile = async (file: File) => {
+      try {
+        await sendFile(channelId, file, handle, filteredMembers);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+  
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { value } = e.target;
+      setInputValue(value);
+      handleInputChange(value);
+    };
+  
+    return (
+      <div className="flex items-center">
+        <ChannelGiphy channelId={channelId} handle={handle} members={members} />
+        <ChannelSendFile onSave={handleSaveFile} channelId={channelId} />
+        <EmojiInput onSubmit={handleSendMessage} handleChange={handleChange} />
+      </div>
+    );
   };
+  
+  export default ChannelInputField;
  
-  const handleSendMessage = async (event) => {
-    event.preventDefault();
+// const ChannelInputField = ({ handle, channelId }) => {
+//   const [message, setMessage] = useState("");
  
-    try {
-      await sendChannelMessage(channelId, message, handle);
-      setMessage("");
-    } catch (error) {
-      console.log(error);
-    }
-  };
+//   const handleInputChange = (event) => {
+//     setMessage(event.target.value);
+//   };
  
-  return (
-    <div className="flex justify-center">
-      <form
-        className="flex w-full max-w-screen-md"
-        onSubmit={handleSendMessage}
-      >
-        <input
-          type="text"
-          placeholder="Send Message"
-          className="w-full border rounded-xl text-start p-2 h-14 my-4"
-          value={message}
-          onChange={handleInputChange}
-        />
-        <button
-          type="submit"
-          className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-xl my-4"
-        >
-          Send
-        </button>
-      </form>
-    </div>
-  );
-};
+//   const handleSendMessage = async (event) => {
+//     event.preventDefault();
  
-export default ChannelInputField;
+//     try {
+//       await sendChannelMessage(channelId, message, handle);
+//       setMessage("");
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+ 
+//   return (
+//     <div className="flex justify-center">
+//       <form
+//         className="flex w-full max-w-screen-md"
+//         onSubmit={handleSendMessage}
+//       >
+//         <input
+//           type="text"
+//           placeholder="Send Message"
+//           className="w-full border rounded-xl text-start p-2 h-14 my-4"
+//           value={message}
+//           onChange={handleInputChange}
+//         />
+//         <button
+//           type="submit"
+//           className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-xl my-4"
+//         >
+//           Send
+//         </button>
+//       </form>
+//     </div>
+//   );
+// };
+ 
+// export default ChannelInputField;
