09b7cff6111bdc50c74fc59e01f4befa69972c5f
Nikolay Hadzhiev <nhadzhiev99@gmail.com>
2023-12-14 18:27:24 +0200

    refactor: Optimize MyMeeting component and update chat video room participant status
    - Remove unused imports and code snippets for better clarity
    - Simplify event handling for roomLeft and roomJoined events
    - Update video room participant status when joining and leaving the video call
    - Implement better error handling and logging for video room status updates

--- WeTalk/src/components/Meeting/MyMeeting.tsx
+++ WeTalk/src/components/Meeting/MyMeeting.tsx
@@ -1,32 +1,79 @@
-import { DyteButton, DyteMeeting } from '@dytesdk/react-ui-kit';
-import { useDyteMeeting, useDyteSelector } from '@dytesdk/react-web-core';
-import { useEffect } from 'react';
+import { DyteMeeting } from '@dytesdk/react-ui-kit';
+import { useDyteMeeting } from '@dytesdk/react-web-core';
+import { useContext, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import Loading from './Loading';
+import {
+  addChatVideoRoomParticipant,
+  deleteChatVideoRoomParticipant,
+  getChatVideoRoomParticipants,
+} from '../../services/chat.service';
+import AppContext from '../../context/AuthContext';
+import { IAppContext } from '../../common/types';
 
-const MyMeeting: React.FC = ({ setIsCallButtonClicked }) => {
+const MyMeeting: React.FC = () => {
   const { meeting } = useDyteMeeting();
   const { chatId } = useParams();
   const navigate = useNavigate();
 
+  const { userData } = useContext(AppContext) as IAppContext;
+
   useEffect(() => {
-    meeting.self.on('roomLeft', () => {
-      console.log(chatId);
-      setIsCallButtonClicked(false);
-      navigate(`/home/chats/${chatId}`);
-    });
-  }, [meeting]);
+    const handleRoomLeft = async () => {
+      try {
+        const latestVideoRoomStatus = await getChatVideoRoomParticipants(
+          chatId!
+        );
 
-  // const roomJoined = useDyteSelector((m) => m.self.roomJoined);
+        if (Object.keys(latestVideoRoomStatus).length > 0) {
+          deleteChatVideoRoomParticipant(chatId!, userData?.handle);
+        }
 
-  // if (!roomJoined) {
-  //   return (
-  //     <div>
-  //       <p>You haven't joined the room yet.</p>
-  //       <DyteButton onClick={() => meeting.joinRoom()}>Join Room</DyteButton>
-  //     </div>
-  //   );
-  // }
+        console.log('left video call');
+        navigate(`/home/chats/${chatId}`);
+      } catch (error) {
+        console.error('Error updating video room status:', error);
+      }
+    };
+
+    const handleRoomJoined = async () => {
+      try {
+        // const latestVideoRoomStatus = await getChatVideoRoomParticipants(
+        //   chatId!
+        // );
+
+        addChatVideoRoomParticipant(chatId!, userData?.handle);
+
+        console.log('joined video call');
+      } catch (error) {
+        console.error('Error updating video from status:', error);
+      }
+    };
+
+    const handleUnload = async () => {
+      // This event is triggered when the user closes the browser/tab
+      console.log('User is leaving the page...');
+      await handleRoomLeft();
+    };
+
+    if (meeting) {
+      meeting.self.on('roomLeft', handleRoomLeft);
+      meeting.self.on('roomJoined', handleRoomJoined);
+    }
+
+    window.addEventListener('beforeunload', handleUnload);
+    window.addEventListener('unload', handleUnload);
+
+    return () => {
+      if (meeting) {
+        meeting.self.off('roomLeft', handleRoomLeft);
+        meeting.self.off('roomJoined', handleRoomJoined);
+      }
+
+      window.removeEventListener('beforeunload', handleUnload);
+      window.removeEventListener('unload', handleUnload);
+    };
+  }, [meeting, chatId, navigate, userData?.handle]);
 
   return (
     <div
