98a3d2e14845814f7fcd88bdbee843e210c4aff2
Nikolay Hadzhiev <nhadzhiev99@gmail.com>
2023-12-14 18:24:00 +0200

    style: Enhance ChatsView component layout and styling
    - Improved overall component structure for better readability
    - Updated background color and styling for a more consistent UI
    - Removed commented-out code and unnecessary console.log statements
    - Adjusted link structure for better navigation
    - Refactored date formatting for better readability in chat previews
    - Applied hover effect for a more interactive user experience

--- WeTalk/src/views/MainSidebar/Chats/ChatsView.tsx
+++ WeTalk/src/views/MainSidebar/Chats/ChatsView.tsx
@@ -29,7 +29,7 @@ const ChatsView = () => {
   useEffect(() => {
     const chatsCallback = (chatsData) => {
       setChats(chatsData);
-      console.log(chatsData);
+      console.log('fetched chats');
     };
 
     const unsubscribe = getAllChats(chatsCallback);
@@ -58,73 +58,88 @@ const ChatsView = () => {
 
   const separateChats = (chatArray) => {
     return (
-      <div>
-        <div>
-          {chatArray.length > 0 ? (
-            chatArray.map((chat) => (
+      <>
+        {chatArray.length > 0 ? (
+          chatArray.map((chat) => (
+            <Link to={`${chat.chatId}`} className="text-gray-500 tracking-wide">
               <div
                 key={chat.chatId}
-                className="flex border bg-blue-500 rounded-lg p-2 my-2 items-center"
+                className="flex bg-blue-500 rounded-lg p-2 pl-1.5 pr-3 my-2 items-center hover:bg-blue-600 justify-between"
               >
-                <Link
-                  to={`${chat.chatId}`}
-                  className="text-gray-500 tracking-wide"
-                >
-                  <div>
-                    <div className="flex flex-row items-center gap-2">
-                      {chat?.members.length > 2 ? (
-                        <p className="pr-2 text-secondary">Group</p>
-                      ) : (
-                        <div>
-                          {chat?.members
+                <div className="flex items-center gap-2">
+                  {chat?.members.length > 2 ? (
+                    <p className="pr-2 text-secondary">Group</p>
+                  ) : (
+                    <div>
+                      {chat?.members
+                        .filter((member) => member?.handle !== userData?.handle)
+                        .map((member) => (
+                          <Profile
+                            key={member?.handle}
+                            handle={member?.handle}
+                          />
+                        ))}
+                    </div>
+                  )}
+                  <div className="flex flex-col text-secondary">
+                    <div className="font-bold text-md">
+                      {chat?.members.length > 2
+                        ? chat?.chatName
+                        : chat?.members[0]?.handle !== userData?.handle
+                        ? `${chat?.members[0]?.firstName} ${chat?.members[0]?.lastName}`
+                        : `${chat?.members[1]?.firstName} ${chat?.members[1]?.lastName}`}
+                    </div>
+                    <div className="text-sm tracking-tight">
+                      {chat?.messages
+                        ? Object.values(chat?.messages)
                             .filter(
-                              (member) => member?.handle !== userData?.handle
+                              (message) => message.sender !== userData?.handle
                             )
-                            .map((member) => (
-                              <Profile
-                                key={member?.handle}
-                                handle={member?.handle}
-                              />
-                            ))}
-                        </div>
-                      )}
-                      <div className="flex flex-col text-secondary">
-                        <div className=" font-bold">
-                          {chat?.members.length > 2
-                            ? chat?.chatName
-                            : chat?.members[0]?.handle !== userData?.handle
-                            ? `${chat?.members[0]?.firstName} ${chat?.members[0]?.lastName}`
-                            : `${chat?.members[1]?.firstName} ${chat?.members[1]?.lastName}`}
-                        </div>
-                        <div>
-                          {chat?.messages
-                            ? Object.values(chat?.messages)
-                                .filter(
-                                  (message) =>
-                                    message.sender !== userData?.handle
-                                )
-                                .slice(-1)
-                                .map((message) => (
-                                  <div>{message.message.slice(0, 17)}...</div>
-                                ))
-                            : null}
-                        </div>
-                      </div>
+                            .slice(-1)
+                            .map((message) => (
+                              <div>
+                                {message.message.slice(0, 25)}
+                                {message.message.length > 25 && <>...</>}
+                              </div>
+                            ))
+                        : null}
                     </div>
                   </div>
-                </Link>
+                </div>
+                <div className="text-xs text-secondary tracking-wide">
+                  {chat?.messages
+                    ? Object.values(chat?.messages)
+                        .filter(
+                          (message) => message.sender !== userData?.handle
+                        )
+                        .slice(-1)
+                        .map((message) => (
+                          <div>
+                            {new Date(message.timestamp).toLocaleTimeString(
+                              'en-US',
+                              {
+                                day: 'numeric',
+                                month: 'short',
+                                hour: 'numeric',
+                                minute: 'numeric',
+                              }
+                            )}
+                          </div>
+                        ))
+                    : null}
+                </div>
               </div>
-            ))
-          ) : (
-            <div>No chats yet!</div>
-          )}
-        </div>
-      </div>
+            </Link>
+          ))
+        ) : (
+          <div>No chats yet!</div>
+        )}
+      </>
     );
   };
 
   return (
-    <div className="min-h-screen sm:min-h-screen md:min-h-screen lg:min-h-screen px-4 py-8 border-r overflow-y-auto bg-secondary w-full sm:w-1/2 md:w-1/2 lg:w-[300px] xl:w-[350px] 2xl:w-[400px] dark:bg-gray-900 dark:border-gray-700">
+    <div className="bg-secondary ml-4 rounded-xl drop-shadow-lg min-h-screen sm:min-h-screen md:min-h-screen lg:min-h-screen px-4 py-4 border-r overflow-y-auto w-full sm:w-1/2 md:w-1/2 lg:w-[300px] xl:w-[500px] 2xl:w-[600px] dark:bg-gray-900 dark:border-gray-700">
       <SearchBar />
       <nav className="mt-6 space-y-4">
         <div className="space-y-2">
@@ -142,13 +157,13 @@ const ChatsView = () => {
           <p className="px-2 text-gray-500 text-sm lg:text-[16px]">
             Personal chats
           </p>
-          <div>{separateChats(personalChats)}</div>
+          {separateChats(personalChats)}
         </div>
         <div>
           <p className="px-2 text-gray-500 text-sm lg:text-[16px]">
             Group chats
           </p>
-          <div>{separateChats(groupChats)}</div>
+          {separateChats(groupChats)}
         </div>
       </nav>
     </div>
