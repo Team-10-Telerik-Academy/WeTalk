058756e2f12df03a768321db8840d544426cdf6a
DianaAlemkova <diana.alemkova@gmail.com>
2023-12-15 04:09:53 +0200

    update: SingleTeam

--- WeTalk/src/components/MainSidebar/Teams/SingleTeam.tsx
+++ WeTalk/src/components/MainSidebar/Teams/SingleTeam.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { getTeamByTeamName } from '../../../services/teams.service';
 import SingleTeamView from '../../../views/MainSidebar/Teams/SingleTeamView';
 import { ITeam, IUserData } from '../../../common/types';
-
+ 
 type ISingleTeamProps = {
   teamName: string;
   onDeleteTeam: (teamName: string, owner: string) => void;
@@ -15,7 +15,7 @@ type ISingleTeamProps = {
   onSaveTeamName: (teamData: ITeam, newName: string) => void;
   users: IUserData[];
 };
-
+ 
 const SingleTeam: React.FC<ISingleTeamProps> = ({
   teamName,
   onDeleteTeam,
@@ -25,7 +25,7 @@ const SingleTeam: React.FC<ISingleTeamProps> = ({
   users,
 }) => {
   const [teamData, setTeamData] = useState<ITeam | null>(null);
-
+ 
   useEffect(() => {
     const fetchTeamData = () => {
       try {
@@ -33,21 +33,21 @@ const SingleTeam: React.FC<ISingleTeamProps> = ({
           if (teamSnapshot && teamSnapshot.exists()) {
             const teamData = teamSnapshot.val();
             setTeamData(teamData);
-            console.log(teamData);
+            console.log('team data fetched');
           }
         });
       } catch (error) {
         console.error('Error fetching team data:', error);
       }
     };
-
+ 
     fetchTeamData();
   }, [teamName]);
-
+ 
   if (!teamData) {
     return <div>Loading...</div>;
   }
-
+ 
   return (
     <>
       <SingleTeamView
@@ -61,5 +61,72 @@ const SingleTeam: React.FC<ISingleTeamProps> = ({
     </>
   );
 };
-
+ 
 export default SingleTeam;
+
+
+// import { useState, useEffect } from 'react';
+// import { getTeamByTeamName } from '../../../services/teams.service';
+// import SingleTeamView from '../../../views/MainSidebar/Teams/SingleTeamView';
+// import { ITeam, IUserData } from '../../../common/types';
+
+// type ISingleTeamProps = {
+//   teamName: string;
+//   onDeleteTeam: (teamName: string, owner: string) => void;
+//   onRemoveMember: (
+//     teamName: string,
+//     owner: string,
+//     memberToRemove: string
+//   ) => void;
+//   onAddMembersToTeam: (teamName: string, members: string[]) => void;
+//   onSaveTeamName: (teamData: ITeam, newName: string) => void;
+//   users: IUserData[];
+// };
+
+// const SingleTeam: React.FC<ISingleTeamProps> = ({
+//   teamName,
+//   onDeleteTeam,
+//   onRemoveMember,
+//   onAddMembersToTeam,
+//   onSaveTeamName,
+//   users,
+// }) => {
+//   const [teamData, setTeamData] = useState<ITeam | null>(null);
+
+//   useEffect(() => {
+//     const fetchTeamData = () => {
+//       try {
+//         getTeamByTeamName(teamName, (teamSnapshot) => {
+//           if (teamSnapshot && teamSnapshot.exists()) {
+//             const teamData = teamSnapshot.val();
+//             setTeamData(teamData);
+//             console.log(teamData);
+//           }
+//         });
+//       } catch (error) {
+//         console.error('Error fetching team data:', error);
+//       }
+//     };
+
+//     fetchTeamData();
+//   }, [teamName]);
+
+//   if (!teamData) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return (
+//     <>
+//       <SingleTeamView
+//         teamData={teamData}
+//         onDeleteTeam={onDeleteTeam}
+//         onRemoveMember={onRemoveMember}
+//         users={users}
+//         onAddMembersToTeam={onAddMembersToTeam}
+//         onSaveTeamName={onSaveTeamName}
+//       />
+//     </>
+//   );
+// };
+
+// export default SingleTeam;
